a5b3bd66183594b9832307221a9f86172ee696a8
Emily <emilylo1381@gmail.com>
2022-07-21 14:50:03 +0800

    updated mapping

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ let energy = 0;
 const thDevices = [];
 const powerDevices = [];
 
-for (let i = 1; i <= TEMP_DEVICE_COUNT; i += 1) {
+for (let i = 0; i < TEMP_DEVICE_COUNT; i += 1) {
 
   let tmp_max = TEMP_MAX;
   let tmp_min = TEMP_MIN;
@@ -40,23 +40,31 @@ for (let i = 1; i <= TEMP_DEVICE_COUNT; i += 1) {
   }
 
   thDevices.push({
-    deviceId: `dummy-temp-${i}`,
+    deviceId: `S${i}`,
     temp: new Simulator((tmp_max + tmp_min) / 2, 1, tmp_min, tmp_max, 2),
     humid: new Simulator((HUMID_MAX + HUMID_MIN) / 2, 5, HUMID_MIN, HUMID_MAX, 2),
   });
 }
 
 for (let i = 1; i <= POWER_DEVICE_COUNT; i += 1) {
+  let tmp = '';
+
+  if(i == 1){
+    tmp = 'lamsoon-main-server-rack-6-KVM1'
+  } else if (i == 2){
+    tmp = 'lamsoon-main-server-rack-6-KVM5'
+  } else if(i == 3){
+    tmp = 'lamsoon-server-ac-C'
+  } else if (i == 2){
+    tmp = 'lamsoon-server-ac-D'
+  }
 
   powerDevices.push({
-    deviceId: `dummy-power-${i}`,
-    vol: new Simulator((VOLTAGE_MAX + VOLTAGE_MIN) / 2, 1, VOLTAGE_MIN, VOLTAGE_MAX, 2), // energy, active power
+    deviceId: `tmp`,
+    vol1: new Simulator((VOLTAGE_MAX + VOLTAGE_MIN) / 2, 1, VOLTAGE_MIN, VOLTAGE_MAX, 2), // energy, active power
     cur1: new Simulator((CURRENT_MAX + CURRENT_MIN) / 2, 5, CURRENT_MIN, CURRENT_MAX, 2),
-    cur2: new Simulator((CURRENT_MAX + CURRENT_MIN) / 2, 5, CURRENT_MIN, CURRENT_MAX, 2),
-    cur3: new Simulator((CURRENT_MAX + CURRENT_MIN) / 2, 5, CURRENT_MIN, CURRENT_MAX, 2),
     pow1: new Simulator((POWER_MAX + POWER_MIN) / 2, 5, POWER_MIN, POWER_MAX, 2),
-    pow2: new Simulator((POWER_MAX + POWER_MIN) / 2, 5, POWER_MIN, POWER_MAX, 2),
-    pow3: new Simulator((POWER_MAX + POWER_MIN) / 2, 5, POWER_MIN, POWER_MAX, 2),
+    totalPow: new Simulator((POWER_MAX + POWER_MIN) / 2, 5, POWER_MIN, POWER_MAX, 2),
     ener: new Simulator((ENERGY_MAX + ENERGY_MIN) / 2, 5, ENERGY_MIN, ENERGY_MAX, 2),
   });
 }
@@ -69,7 +77,7 @@ function generateData() {
     const humidity = humid.generate();
 
     client.publish(
-      `site-a/data/${deviceId}/ambient`,
+      `lamsoon/data/${deviceId}/ambient`,
       JSON.stringify({
         timestamp,
         deviceId,
@@ -79,29 +87,23 @@ function generateData() {
     );
   });
 
-  powerDevices.forEach(({ deviceId, vol, cur1, cur2, cur3, pow1, pow2, pow3, ener }) => {
-    const voltage = vol.generate();
+  powerDevices.forEach(({ deviceId, vol1, cur1,  pow1, totalPow, ener }) => {
+    const voltage = vol1.generate();
     const current1 = cur1.generate();
-    const current2 = cur2.generate();
-    const current3 = cur3.generate();
     const power1 = pow1.generate();
-    const power2 = pow2.generate();
-    const power3 = pow3.generate();
+    const totalPower = totalPow.generate();
     const e = ener.generate();
     energy += e;
 
     client.publish(
-      `site-a/data/${deviceId}/power`,
+      `lamsoon/data/${deviceId}/power`,
       JSON.stringify({
         timestamp,
         deviceId,
         voltage,
         current1,
-        current2,
-        current3,
         power1,
-        power2,
-        power3,
+        totalPower,
         energy
       }),
     );
